241cd816d722c31ab0d2f9b0d3c2d3eb5b5e852a
Ken Mutuku <you@example.com>
2025-06-09 11:28:21 +0300

    to the 1and a half mark

---
+++ Frontend/src/context/Storecontext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+import { food_list } from "../assets/assets";
+export const StoreContext= createContext(null)
+
+const StoreContextProvider =(props)=>{
+    const {cartItems,setCartItems}=useState({});
+    const addToCart= (itemId) =>{
+        if(!cartItems[itemId]){
+            setCartItems()
+        }
+    }
+
+    const contextValue= {
+        food_list
+    }
+    return(
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+
+    )
+}
+ 
+export default StoreContextProvider
