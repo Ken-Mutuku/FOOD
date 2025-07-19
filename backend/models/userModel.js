2b2643137c721f09dad636fb5c228b0e343acfa9
Ken Mutuku <you@example.com>
2025-07-19 17:03:24 +0300

    Back on track

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,10 @@ const userSchema = new mongoose.Schema({
     name: {type: String,requried: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    cartData:{
+        type: Object,
+        default: {}
+    }
 },{minimize:false})////minimize:false allows empty objects to be stored in the database
  
 const userModel = mongoose.model.users || mongoose.model('users', userSchema);//if the model already exists, use it, otherwise create a new one
