87cb7a6aecd9a38dfab14277c0fe114bacbddc36
Ken Mutuku <you@example.com>
2025-08-12 11:03:09 +0300

    9.04

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import authMiddlware from "../middleware/auth.js";
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, verifyOrder ,userOrders} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
-
+//authmiddleware is used to protect the routes and where user id is required
 orderRouter.post("/place", authMiddlware, placeOrder);
 orderRouter.post("/verify", verifyOrder); // Assuming you have a verifyOrder function
+orderRouter.post("/userorders", authMiddlware, userOrders); // Route to get user orders
   
 
 export default orderRouter;
